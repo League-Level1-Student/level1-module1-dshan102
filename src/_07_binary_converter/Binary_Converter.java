466af853a4a9d723ce5cdb3b248921ab8f87070a
League Student <league@iMac-22.attlocal.net>
2020-03-06 16:03:34 -0800

    Binary Converter

--- src/_07_binary_converter/Binary_Converter.java
+++ src/_07_binary_converter/Binary_Converter.java
@@ -1,17 +1,22 @@
 package _07_binary_converter;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Binary_Converter {
+public class Binary_Converter implements MouseListener {
 
-	JFrame frame = new JFrame();
+	JFrame frame = new JFrame("Convert 8 bits of binary to ASCII");
 	JPanel panel = new JPanel();
-	JTextField answer= new JTextField(20);
-	JButton button = new JButton();
+	JLabel label = new JLabel();
+	JTextField input = new JTextField(20);
+	JButton button = new JButton("Convert");
 	
 	public static void main(String[] args) {
 	
@@ -21,13 +26,13 @@ public class Binary_Converter {
 	}
 
 public void createUI(){
-	frame.isVisible();
-	
-	panel.isVisible();
-	answer.isVisible();
-
-	button.isVisible();
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(input);
+	panel.add(button);
+	panel.add(label);
 	button.addMouseListener(this);
+
 	
 }
 
@@ -49,6 +54,39 @@ String convert(String input) {
         JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
         return "-";
     }
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+	
+	String binaryFormat = convert(input.getText());
+	label.setText(binaryFormat);
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 }
 
